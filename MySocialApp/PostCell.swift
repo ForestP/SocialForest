e6d04aa398ed2ed5d9f8811b94f6d627f8284a82
Forest Plasencia <ForestP3@Forests-Air.attlocal.net>
2017-01-06 14:33:31 -0600

    Updated feed table UI

---
+++ MySocialApp/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  MySocialApp
+//
+//  Created by Forest Plasencia on 1/6/17.
+//  Copyright © 2017 Forest Plasencia. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+
+}
